ad191cd84a9871ec37472260e323f029a1037db2
ducvnkm2k4 <duckmhd2k4@gmail.com>
2024-12-25 00:08:44 +0700

    firebase config

--- src/configs/firebase_config.js
+++ src/configs/firebase_config.js
@@ -5,7 +5,7 @@ dotenv.config();
 const serviceAccount = {
     "type": process.env.FIREBASE_TYPE,
     "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID.replace(/\\n/g, '\n'),
     "private_key": process.env.FIREBASE_PRIVATE_KEY,
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     "client_id": process.env.FIREBASE_CLIENT_ID,
@@ -15,6 +15,7 @@ const serviceAccount = {
     "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
     "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
 }
+console.log(serviceAccount);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 })
